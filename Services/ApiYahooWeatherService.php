1a1e62c00469ef9a3446541abace58a1d31c54a2
seo <seo@seo>
2014-11-10 15:48:48 +0100

    nothing

--- Services/ApiYahooWeatherService.php
+++ Services/ApiYahooWeatherService.php
@@ -27,7 +27,6 @@ class ApiYahooWeatherService {
     /** @var string $unit c or f for Celsius or Fahrenheit */
     protected $unit;
 
-<<<<<<< HEAD
     /**
      * Constructor
      * 
@@ -39,8 +38,6 @@ class ApiYahooWeatherService {
      * 
      * @return void
      */
-=======
->>>>>>> 54a80174388619f151ab4548908c275f3166f5be
     public function __construct($woeid, $unit, \Aequasi\Bundle\CacheBundle\Service\CacheService $memcached = NULL, $ttl, $env) {
         $this->memcached = $memcached;
         $this->ttl = $ttl;
@@ -131,5 +128,4 @@ class ApiYahooWeatherService {
     public function getForecast(){
         return $this->apiYahooWeatherObject->getForecast();
     }
-
 }
